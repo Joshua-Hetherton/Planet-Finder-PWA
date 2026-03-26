679a6a0fd640f8514224b2afe370631699957c1c
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-03-26 09:31:25 +0000

    Astronomy-Engine Setup

---
+++ public/js/astronomy-calc.js
@@ -0,0 +1,32 @@
+import * as Astronomy from 'astronomy-engine';
+
+const planet_names= {
+    'Mercury': Astronomy.Body.Mercury,
+    'Venus': Astronomy.Body.Venus,
+    'Earth': Astronomy.Body.Earth,
+    'Mars': Astronomy.Body.Mars,
+    'Jupiter': Astronomy.Body.Jupiter,
+    'Saturn': Astronomy.Body.Saturn,
+    'Uranus': Astronomy.Body.Uranus,
+    'Neptune': Astronomy.Body.Neptune,
+}
+
+function getPlanetPosition() {
+    
+}
+
+/*
+gets rise, set and transit time when given a planet, date and location
+*/
+function getPlanetPositionTime() {
+
+}
+
+/*
+Gets all currently visible planets from a given location
+*/
+function getVisiblePlanets() {
+
+}
+
+
