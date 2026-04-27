19e0a14288edeb55357ea8d55bc95241bd50828d
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-04-27 11:39:02 +0100

    Feat: Intergrating Astronomy Engine Outputs. Console.log Only

--- public/js/astronomy-calc.js
+++ public/js/astronomy-calc.js
@@ -1,26 +1,25 @@
-import * as Astronomy from 'astronomy-engine';
+
 
 const planet_names= {
-    'Mercury' : Astronomy.Body.Mercury,
-    'Venus': Astronomy.Body.Venus ,
-    'Earth':  Astronomy.Body.Earth,
-    'Mars': Astronomy.Body.Mars,
-    'Jupiter': Astronomy.Body.Jupiter,
-    'Saturn': Astronomy.Body.Saturn,
-    'Uranus': Astronomy.Body.Uranus,
-    'Neptune' : Astronomy.Body.Neptune ,
+    'mercury' : Astronomy.Body.Mercury,
+    'venus': Astronomy.Body.Venus ,
+    'earth':  Astronomy.Body.Earth,
+    'mars': Astronomy.Body.Mars,
+    'jupiter': Astronomy.Body.Jupiter,
+    'saturn': Astronomy.Body.Saturn,
+    'uranus': Astronomy.Body.Uranus,
+    'neptune' : Astronomy.Body.Neptune ,
 }
 /*
 Gets the current position of the planet, when given a time and location. 
 Returns the position as an Azmiuth and Altitude to allow easy telescope pointing.
 */
-
 function getPlanetPosition(latitude, longitude, date, planet_name) {
     /*Creates an observer, calculates the rest of the code given the users location.
     Height is set to 0 by default, unless a user wants to see planets high above sea level*/
-    const observer= new Astronomy.Observer( latitude, longitude, height=0);
+    const observer= new Astronomy.Observer( latitude, longitude, 0);
 
-    const body= bodyMap[planet_name.tolowerCase()];
+    const body= planet_names[planet_name.toLowerCase()];
 
     const equator=Astronomy.Equator(body, date, observer, true, true);
     const Horizon=Astronomy.Horizon(date, observer, equator.ra, equator.dec, 'normal');
@@ -46,36 +45,36 @@ function getPlanetPosition(latitude, longitude, date, planet_name) {
 /*
 gets rise, set and transit time when given a planet, date and location
 */
-function getPlanetPositionTime(latitude, longitude, date, planet_name) {
+    function getPlanetObserveableTime(latitude, longitude, date, planet_name) {
 
-    const observer= new Astronomy.Observer( latitude, longitude, height=0);
+        const observer= new Astronomy.Observer( latitude, longitude, 0);
 
-    const body= bodyMap[planet_name.tolowerCase()];
+        const body= planet_names[planet_name.toLowerCase()];
 
-    /*Gets rise time 
-    direction=1 refers to the east*/
-    const rise_time= Astronomy.SearchRiseSet(body, observer, datestart=date, direction=1, horizon=0);
-    /*Gets rise time 
-    direction=-1 refers to the west*/
-    const set_time=Astronomy.SearchRiseSet(body, observer, datestart=date, direction=-1, horizon=0);
+        /*Gets rise time 
+        direction=+1 refers to the east*/
+        const rise_time= Astronomy.SearchRiseSet(body, observer, date, +1, 0);
+        /*Gets rise time 
+        direction=-1 refers to the west*/
+        const set_time=Astronomy.SearchRiseSet(body, observer, date, -1, 0);
 
-    /*Gets transit time, how long it will be visible*/
-   const transit_time=Astronomy.SearchHourAngle(body, observer, datestart=date, direction=1, horizon=0);
+        /*Gets transit time, how long it will be visible*/
+    const transit_time=Astronomy.SearchHourAngle(body, observer, +1, date);
 
-   //Finding culmination time, when it will be at its highest point in the sky (when the hour angle is 0)
-   const culmination_time= Astronomy.SearchHourAngle(body, observer,hourAngle=0, datestart=date);
+    //Finding culmination time, when it will be at its highest point in the sky (when the hour angle is 0)
+    const culmination_time= Astronomy.SearchHourAngle(body, observer,0, date);
 
-   return {
-    planet: planet_name,
-    date: date.toISOString(),
-    rise_time: rise_time.toISOString(),
-    set_time: set_time.toISOString(),
-    transit_time: transit_time.toISOString(),
-    culmination_time: culmination_time.toISOString()
+    return {
+        planet: planet_name,
+        date: date.toISOString(),
+        rise_time: rise_time,
+        set_time: set_time,
+        transit_time: transit_time,
+        culmination_time: culmination_time
 
-   }
+    }
 
-}
+    }
 
 /*
 Gets all currently visible planets from a given location
@@ -83,9 +82,8 @@ Gets all currently visible planets from a given location
 */
 function getVisiblePlanets(latitude, longitude, date) {
 
-    const observer= new Astronomy.Observer( latitude, longitude, height=0);
+    const observer= new Astronomy.Observer( latitude, longitude, 0);
 
-    const body= bodyMap[planet_name.tolowerCase()];
 
     const visible_planets= [];
 
@@ -133,5 +131,4 @@ function findNextEclipse(date) {
     }
 }
 
-export {getPlanetPosition, getPlanetPositionTime, getVisiblePlanets, moonPhase, findNextEclipse};
 
