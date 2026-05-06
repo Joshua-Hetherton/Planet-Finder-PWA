c779e3389f9daf38f4b765e0accfc3f57ed61f75
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-05-06 10:41:10 +0100

    Feat: Added filled Event Listerners for Calender. Calendar now displays days in line with actual calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,47 +1,66 @@
 let current_date=new Date();
-let currnent_month=current_date.getMonth();
+let current_month=current_date.getMonth();
 let current_year=current_date.getFullYear();
 
 async function updateCalendarMonthTitle() {
-    const month_title=document.getElementById("")
+    const month_title=document.getElementById("Calendar-title");
     const month_name= current_date.toLocaleString("default", {month:"long"});
 
     month_title.textContent= `${month_name}, ${current_year}`;
 }
 
 async function generateCalendar() {
-    const grid=document.getElementById("calendar-grid").innerHTML="";
+    const grid=document.getElementById("grid-calendar");
+    grid.innerHTML="";
     //Gets days in month by using getDate() to look at first day of new month
-    const month_days= new Date(current_year, currnent_month + 1, 0).getDate();
+    const month_days= new Date(current_year, current_month + 1, 0).getDate();
     const first_day= new Date(current_year, current_month, 1);
 
     //Adjusts start day to match calendar format, putting blank spaces in before first day of the month
-    let start_day_index=first_day.getDay()+6 % 7;
+    let start_day_index=(first_day.getDay()+6 )% 7;
 
     for(i=0; i<start_day_index; i++) {
-        const empty_cell=document.createElement("div").classList.add("day", "empty");
+        const empty_cell=document.createElement("div");
+        empty_cell.classList.add("day", "empty");
         grid.appendChild(empty_cell);
     }
 
     //Filling calendar with days
     for( day=1; day<=month_days; day++) {
-        const day_cell=document.createElement("div").classList.add("day");
+        const day_cell=document.createElement("div");
+        day_cell.classList.add("day");
         day_cell.innerHTML=`<div class="date"> ${day} </div>`;
         grid.appendChild(day_cell);
     }
+
+    updateCalendarMonthTitle();
 }
 
 
 document.getElementById("Previous-Month").addEventListener("click", () => {
-
+    current_month--;
+    if (current_month <0) {
+        current_month=11;
+        current_year--;
+    }
+    generateCalendar();
 });
 
 document.getElementById("Next-Month").addEventListener("click", () => {
-
+    current_month++;
+    if (current_month >11) {
+        current_month=0;
+        current_year++;
+    }
+    generateCalendar();
 });
 
 document.getElementById("Today").addEventListener("click", () => {
+    current_date=new Date()
+    current_Month=current_date.getMonth();
+    current_year=current_date.getFullYear();
 
+    generateCalendar();
 });
 
 document.getElementById("submit-information").addEventListener("click", () => {
