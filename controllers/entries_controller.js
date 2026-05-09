908ec04b75aabb31ff5a056ca6c780fa238a6df8
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-05-09 14:31:25 +0100

    Added entry Controller

--- controllers/entries_controller.js
+++ controllers/entries_controller.js
@@ -1,17 +1,68 @@
 const Day=require("../models/day");
 
 exports.getEntry= async(req, res) => {
-
+    Day.find().then((entries) => {
+        res.status(200).json(entries);
+    })
+    .catch((error) => {
+        res.status(500).json({error: "Error fetching the entries"});
+    });
 }
 
 exports.createEntry= async(req, res) => {
-    
+    const entry= new Day({
+        user_id: req.body.user_id,
+        date: req.body.date,
+        planet_observed: req.body.planet_observed,
+        equipment_used: req.body.equipment_used,
+        viewing_location: req.body.viewing_location,
+        user_notes: req.body.user_notes
+    })
+    entry.save()
+    .then((savedEntry) => {
+        res.status(201).json(savedEntry);
+    })
+    .catch((error) => {
+        res.status(500).json({error: "Error creating the entry"});
+    });
 }
 
 exports.updateEntry= async(req, res) => {
+    const date=req.params.date;
+
+    Day.findOne({date:date}).then((entry) => {
+        if(!entry) {
+            return res.status(404).json({error: "Entry not found"});
+        }
+        user_id= req.body.user_id,
+        date= req.body.date,
+        planet_observed= req.body.planet_observed,
+        equipment_used= req.body.equipment_used,
+        viewing_location= req.body.viewing_location,
+        user_notes= req.body.user_notes
+        entry.save();
+    })
+    .then((savedEntry) => {
+        res.status(201).json(savedEntry);
+    })
+    .catch((error) => {
+        res.status(500).json({error: "Error creating the entry"});
+    });
     
 }
 
 exports.deleteEntry= async(req, res) => {
-    
+    const date=req.params.date;
+
+    Day.deleteOne({date:date}).then((result) => {
+        if(!(result.deletedCount ===0)) {
+            res.status(200).json({message: "Entry Deleted"});
+        }
+        else {
+            res.status(404).json({error: "Entry not found"});
+        }
+    })
+    .catch((error) => {
+        res.status(500).json({error: "Error deleting the entry"});
+    });
 }
